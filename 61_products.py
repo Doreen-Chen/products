87651265fab42632ff271d9f7a275309765b323d
Doreen Chen <doreen.chen9@gmail.com>
2021-06-28 16:52:46 +0800

    add read file

--- 61_products.py
+++ 61_products.py
@@ -1,10 +1,33 @@
 # 61. 建立記帳程式專案 (+二維清單 2 dimension)
 # & 62. 寫入檔案
 # & 63. 型別轉換提點
+# & 64. 寫入欄位名稱 + 編碼問題
+# & 65. 讀取檔案 + split()
+# & 66. Continue - 跳過欄位名稱
 #
 # 讓使用者重複輸入(購買過的)商品名稱, 不清楚購買商品種類,所以使用while loop
 
+
 products = []
+
+#####  65. 讀取檔案 + split()
+with open('products.csv', 'r', encoding='utf-8') as f:
+    for line in f:
+        # 66. Continue - 跳過欄位名稱
+        if '商品,價格' in line:
+            continue  # 跳過欄位名稱, 下一筆
+        ### 原始寫法
+        #s = line.strip().split(',')  ### split 切割後是清單(s)
+        #name = s[0]
+        #price = s[1]
+        ### 快寫法
+        name, price = line.strip().split(',')
+        #print('商品:' + name + ' 價格:' + price)
+        products.append([name, price])  # 加進list裡
+
+print(products)
+
+# 讓使用者輸入
 while True:
     name = input('請輸入商品名稱 :')
     if name == 'q':  # quit
@@ -23,10 +46,10 @@ print(products)
 # 存取二維清單中指定位置
 print('清單第2項商品的價格', str(products[1][1]))
 
+# 印出所有購買記錄
 for p in products:
     #print('您購買的是', p[0], ', 價格是', p[1])
     print('您購買的是', p[0], ', 價格是', str(p[1])) # 價格已在置入list時換成int型態
-
 
 # 補充:字串的加法及乘法
 #'abc' + '123' = 'abc123'
@@ -36,17 +59,17 @@ for p in products:
 #####  62. 寫入檔案
 #    參考 52. 讀取檔案
 #####  64. 寫入欄位名稱 + 編碼問題
-#    檔案寫入跟讀取都與編碼有關
+#    檔案寫入跟讀取都與編碼有關 
+#       (到課程64, open('w') 是寫入新的檔案,原始資料會被覆蓋,所以課程65在讓使用者輸入前,先讀取檔案 )
 #    在開檔open()時, 要指定編號 UTF-8
 
-# (1) txt
+# 寫入檔案-(1) txt
 with open('products.txt', 'w') as f:
     for p in products:
         f.write('商品 : ' + p[0] + ', 價格是 ' + str(p[1]) + '\n') # 價格已在置入list時換成int型態
 
-# (2) csv
+# 寫入檔案-(2) csv
 with open('products.csv', 'w', encoding='utf-8') as f:
     f.write('商品,價格\n')  # 64. 寫入欄位名稱 + 編碼問題
     for p in products:
         f.write(p[0] + ',' + str(p[1]) + '\n') # 價格已在置入list時換成int型態
-
